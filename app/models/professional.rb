96bc7c5b262185bdc985557f99ff39468300b974
kesonother <kesonother@gmail.com>
2011-08-04 23:05:26 +0200

    modification affichage des lien de connexion

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,8 +1,13 @@
-class Profesionnal
+class Professional
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  field :first_name
+  field :last_name
+  
+  validates_uniqueness_of :email
 
 end
