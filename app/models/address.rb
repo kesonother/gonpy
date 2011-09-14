861eea73c84cea2bcaca2c212ae2cc10d8fd50b0
kesonother <kesonother@gmail.com>
2011-09-14 19:20:19 +0200

    update model adress et views _form_edit_name

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,13 +4,26 @@ class Address
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
   
+  field :street, :type => String  
+  field :city, :type => String
+  field :zipcode, :type => Integer
+  field :coordinates, :type => Array
   field :longitude, :type => Float
   field :latitude, :type => Float
-  field :coordinates, :type => Array
   field :address
   
   validates_numericality_of :zipcode, even: true
   embedded_in :proaccount
 
-
+  def address
+    [street, city, zipcode].compact.join(', ')
+  end
+  
+  def latitude
+    coordinates[0]
+  end
+  
+  def longitude
+    coordinates[1]
+  end
 end
